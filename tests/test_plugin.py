8756129045be3d8d7b9bd7014ed656d87be9302f
Alan Hamlett <alan.hamlett@gmail.com>
2017-06-07 21:16:32 -0700

    fix tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -8,15 +8,15 @@ Main plugin tests.
 """
 import unittest
 from mock import Mock
-from ..plugin import WatchPlugin
+from nosewatch.plugin import WatchPlugin
 
 
 class TestWatchPlugin(unittest.TestCase):
 
     def setUp(self):
         self.plugin = WatchPlugin()
-        self.plugin.sys = Mock()
-        self.plugin.sys.argv = ['program', 'arg1', '--with-watch', 'arg3', 'arg4']
+        self.plugin.stdout = Mock()
+        self.plugin.argv = ['program', 'arg1', '--with-watch', 'arg3', 'arg4']
 
     def test_finalize(self):
         self.plugin.call = Mock()
@@ -25,10 +25,10 @@ class TestWatchPlugin(unittest.TestCase):
         watchcmd = 'clear && program arg1 arg3 arg4'
         self.plugin.call.assert_called_once_with([
             'watchmedo', 'shell-command', '-c', watchcmd,
-            '-R', '-p', '*.py', '.'])
+            '-R', '-p', '*.py', '-W', '.'])
 
     def test_finalize_interrupted(self):
         self.plugin.call = Mock(side_effect=KeyboardInterrupt)
         self.plugin.finalize(Mock())
-        self.plugin.sys.stdout.write.assert_called_once_with('\nStopped\n')
+        self.plugin.stdout.write.assert_called_once_with('\nStopped\n')
 
